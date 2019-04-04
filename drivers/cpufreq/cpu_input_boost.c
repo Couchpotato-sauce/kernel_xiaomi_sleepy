5f0eff9f33300b602336d5ec82fbe7e0b73d903e
Danny Lin <danny@kdrag0n.dev>
2019-04-03 18:05:21 -0700

    cpu_input_boost: Track input event times and expose timeout helper
    Sometimes we may want to limit certain boost kicks to only take effect
    within a certain timeout after receiving the last input event to save
    power. For example, we may want to boost when an intensive process
    forks to improve fluidity, but boosting when the fork was not initiated
    by input is wasteful.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -51,6 +51,7 @@ struct boost_drv {
 	wait_queue_head_t boost_waitq;
 	atomic64_t max_boost_expires;
 	atomic_t state;
+	unsigned long last_input_jiffies;
 };
 
 static struct boost_drv *boost_drv_g __read_mostly;
@@ -106,6 +107,17 @@ static void update_online_cpu_policy(void)
 	for_each_online_cpu(cpu)
 		cpufreq_update_policy(cpu);
 	put_online_cpus();
+}
+
+bool cpu_input_boost_within_input(unsigned long timeout_ms)
+{
+	struct boost_drv *b = boost_drv_g;
+
+	if (!b)
+		return true;
+
+	return time_before(jiffies, b->last_input_jiffies +
+			   msecs_to_jiffies(timeout_ms));
 }
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
@@ -274,6 +286,8 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	struct boost_drv *b = handle->handler->private;
 
 	__cpu_input_boost_kick(b);
+
+	b->last_input_jiffies = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
@@ -364,6 +378,7 @@ static int __init cpu_input_boost_init(void)
 	init_waitqueue_head(&b->boost_waitq);
 	atomic64_set(&b->max_boost_expires, 0);
 	atomic_set(&b->state, 0);
+	b->last_input_jiffies = jiffies;
 
 	b->cpu_notif.notifier_call = cpu_notifier_cb;
 	ret = cpufreq_register_notifier(&b->cpu_notif, CPUFREQ_POLICY_NOTIFIER);
