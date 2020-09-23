b25f8651fff83dbda803341a9afe9a0a2fdfed5e
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-09-23 21:41:14 +0530

    camera_v2: disable actuator lens parking for daisy
    fixes actuator power cycling during initialization
    
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1132,12 +1132,14 @@ static int32_t msm_actuator_power_down(struct msm_actuator_ctrl_t *a_ctrl)
 	CDBG("Enter\n");
 	if (a_ctrl->actuator_state != ACT_DISABLE_STATE) {
 
+#ifndef CONFIG_MACH_XIAOMI_DAISY
 		if (a_ctrl->func_tbl && a_ctrl->func_tbl->actuator_park_lens) {
 			rc = a_ctrl->func_tbl->actuator_park_lens(a_ctrl);
 			if (rc < 0)
 				pr_err("%s:%d Lens park failed.\n",
 					__func__, __LINE__);
 		}
+#endif
 
 		rc = msm_actuator_vreg_control(a_ctrl, 0);
 		if (rc < 0) {
