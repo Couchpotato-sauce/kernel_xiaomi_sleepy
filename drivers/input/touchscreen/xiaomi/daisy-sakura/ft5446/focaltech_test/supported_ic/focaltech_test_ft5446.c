fbbd3075871d0bb6cd06ab49e2c7af5609956e5c
WJXXBSH <zyb_1998@outlook.com>
2020-11-08 14:17:35 +0800

    drivers: input: touchscreen: ft5446: Remove characters with big5 encoding
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/focaltech_test_ft5446.c:1814:42: warning: illegal character encoding in string literal [-Winvalid-source-encoding]
                                    FTS_TEST_SAVE_INFO("\nTx%02d: %02d (k<A6><B8>),       ", i + 1, fGShortResistance[i]);
                                                                         ^~~~~~~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/../focaltech_test.h:335:3: note: expanded from macro 'FTS_TEST_SAVE_INFO'
                    fmt, ##args);\
                    ^~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/focaltech_test_ft5446.c:1816:42: warning: illegal character encoding in string literal [-Winvalid-source-encoding]
                                    FTS_TEST_SAVE_INFO("\nRx%02d: %02d (k<A6><B8>),       ", i + 1 - iMaxTx, fGShortResistance[i]);
                                                                         ^~~~~~~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/../focaltech_test.h:335:3: note: expanded from macro 'FTS_TEST_SAVE_INFO'
                    fmt, ##args);\
                    ^~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/focaltech_test_ft5446.c:1868:41: warning: illegal character encoding in string literal [-Winvalid-source-encoding]
                                    FTS_TEST_SAVE_INFO("\nTx%02d: %02d(k<A6><B8>),        ", i + 1, fMShortResistance[i]);
                                                                        ^~~~~~~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/../focaltech_test.h:335:3: note: expanded from macro 'FTS_TEST_SAVE_INFO'
                    fmt, ##args);\
                    ^~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/focaltech_test_ft5446.c:1870:41: warning: illegal character encoding in string literal [-Winvalid-source-encoding]
                                    FTS_TEST_SAVE_INFO("\nRx%02d: %02d(k<A6><B8>),        ", i + 1 - iMaxTx, fMShortResistance[i]);
                                                                        ^~~~~~~~
    ../drivers/input/touchscreen/ft5446_sakura/focaltech_test/supported_ic/../focaltech_test.h:335:3: note: expanded from macro 'FTS_TEST_SAVE_INFO'
                    fmt, ##args);\
                    ^~~
    
    Change-Id: I64e86cc0e82baf3cbdc8dcb3d376eb5a7b7cacd4
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- drivers/input/touchscreen/xiaomi/daisy-sakura/ft5446/focaltech_test/supported_ic/focaltech_test_ft5446.c
+++ drivers/input/touchscreen/xiaomi/daisy-sakura/ft5446/focaltech_test/supported_ic/focaltech_test_ft5446.c
@@ -1811,9 +1811,9 @@ unsigned char FT5X46_TestItem_WeakShortTest( bool *bTestResult )
 		if ((iMin_CG > fGShortResistance[i]) || (iDsen - iDoffset < 0)) { //<=0 ShortResistance = 0
 			iCount++;
 			if (i + 1 <= iMaxTx)
-				FTS_TEST_SAVE_INFO("\nTx%02d: %02d (k次),	", i + 1, fGShortResistance[i]);
+				FTS_TEST_SAVE_INFO("\nTx%02d: %02d (k times),	", i + 1, fGShortResistance[i]);
 			else
-				FTS_TEST_SAVE_INFO("\nRx%02d: %02d (k次),	", i + 1 - iMaxTx, fGShortResistance[i]);
+				FTS_TEST_SAVE_INFO("\nRx%02d: %02d (k times),	", i + 1 - iMaxTx, fGShortResistance[i]);
 			if (iCount % 10 == 0)
 				FTS_TEST_SAVE_INFO("\n\n");
 		}
@@ -1865,9 +1865,9 @@ unsigned char FT5X46_TestItem_WeakShortTest( bool *bTestResult )
 		if ( fMShortResistance[i] <= 0  || fMShortResistance[i] < iMin_CC ) {
 			iCount++;
 			if (i + 1 <= iMaxTx)
-				FTS_TEST_SAVE_INFO("\nTx%02d: %02d(k次),	", i + 1, fMShortResistance[i]);
+				FTS_TEST_SAVE_INFO("\nTx%02d: %02d(k times),	", i + 1, fMShortResistance[i]);
 			else
-				FTS_TEST_SAVE_INFO("\nRx%02d: %02d(k次),	", i + 1 - iMaxTx, fMShortResistance[i]);
+				FTS_TEST_SAVE_INFO("\nRx%02d: %02d(k times),	", i + 1 - iMaxTx, fMShortResistance[i]);
 
 			if (iCount % 10 == 0)
 				FTS_TEST_SAVE_INFO("\n\n");
