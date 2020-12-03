eea2aa52355092a43d775f364118394483810d79
Lyceris-chan <ghostdrain@outlook.com>
2020-12-04 00:53:28 +0100

    drivers: input: touchscreen: daisy-sakura: disable debugging and testing

--- drivers/input/touchscreen/xiaomi/daisy-sakura/ft5446/focaltech_config.h
+++ drivers/input/touchscreen/xiaomi/daisy-sakura/ft5446/focaltech_config.h
@@ -100,7 +100,7 @@
  * show debug log info
  * enable it for debug, disable it for release
  */
-#define FTS_DEBUG_EN                            1
+#define FTS_DEBUG_EN                            0
 
 /*
  * Linux MultiTouch Protocol
@@ -130,7 +130,7 @@
  * Production test enable
  * 1: enable, 0:disable(default)
  */
-#define FTS_TEST_EN                             1
+#define FTS_TEST_EN                             0
 
 /*
  * Glove mode enable
