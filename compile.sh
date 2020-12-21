7fbb2bcea64e23f2852740c55db3dc72faa72813
lyceris-chan <ghostdrain@outlook.com>
2020-12-21 18:36:55 +0100

    compile.sh: add lacia in japanese as well smh

--- compile.sh
+++ compile.sh
@@ -62,7 +62,7 @@ incremental() {
     fi
 }
 
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     incremental
 else
     sleep 10
@@ -73,7 +73,7 @@ wireguard() {
     ./scripts/fetch-latest-wireguard.sh
 }
 
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     wireguard
 else
     sleep 10
@@ -93,7 +93,7 @@ export PATH
 export KBUILD_COMPILER_STRING
 
 # Git log the last 10 commits and upload them to del.dog (Lacia only)
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     git log --pretty=oneline -10 >changelog.txt
     pastebinit changelog.txt >changes.txt
     CHANGELOG=$(cat changes.txt)
@@ -118,7 +118,7 @@ TelegramAPI() {
     fi
 }
 
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     TelegramAPI
 else
     sleep 10
@@ -154,7 +154,7 @@ Changelog [Here!]($CHANGELOG)
 Kernel: \`Sleepy\`"
 }
 
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     TelegramInfo
 else
     sleep 10
@@ -220,7 +220,7 @@ TelegramStatus() {
     fi
 }
 
-if [ "$(whoami)" = "lacia" ] || [ "$(whoami)" = "lacia-chan" ]; then
+if [ "$(whoami)" = "lacia" ]  || [ "$(whoami)" = "lacia-chan" ] || [ "$(whoami)" = "レイシア" ]; then
     TelegramStatus
 else
     FILE="$(pwd)/out/arch/arm64/boot/Image.gz-dtb"
