d24b7f02082610b8ec2e0ffd046ff8c92b212052
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-05-07 22:25:19 +0000

    fs: open: Add some more apps and modules to the blocker list

--- fs/open.c
+++ fs/open.c
@@ -1081,9 +1081,12 @@ static char *files_array[] = {
 	"com.feravolt",
 	"fde",
 	"lspeed",
+    	"com.paget96.lsandroid",
 	"nfsinjector",
 	"lkt",
 	"MAGNE",
+	"vulmax",
+	"vgaming"
 };
 
 static char *paths_array[] = {
